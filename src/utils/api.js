d2d7e7022969c898beae32c5aa82b3bdfa7be8a6
Ravishankaradhikari <ravishankaradhikari10@gmail.com>
2023-09-19 14:56:00 +0545

    ravi

---
+++ src/utils/api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube138.p.rapidapi.com";
+
+const options = {
+    params: { hl: "en", gl: "US" },
+    headers: {
+        "X-RapidAPI-Key":
+            process.env.REACT_APP_YOUTUBE_API_KEY || "07b8f1d7efmshd708af424005496p1bbf65jsn35f14b4e1dfd",
+        "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+    },
+};
+
+export const fetchDataFromApi = async (url) => {
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+    return data;
+};
